a05882e46ce4241c69dab4453a92dd5278edbabb
piyush goyal <piyush2022ug@gmail.com>
2025-05-12 11:29:16 +0530

    feat: movie by ID

--- server.js
+++ server.js
@@ -278,6 +278,17 @@ app.get("/movies", async (req, res) => {
   }
 });
 
+app.get("/movies/:id", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
 app.listen(3002, () => {
   console.log("Server is running on 3002");
 });
